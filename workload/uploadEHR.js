ae89fc785cd47bf142b496f611123c2aca2c6d7e
ZihuaZhang <1324088931@qq.com>
2024-03-25 01:15:30 +0800

    mm

--- workload/uploadEHR.js
+++ workload/uploadEHR.js
@@ -9,10 +9,10 @@ class MyWorkload extends WorkloadModuleBase {
 
     async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
         await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
-        this.pk = fs.readFileSync('../pk.txt','utf8') 
+        this.pk = fs.readFileSync('pk.txt','utf8') 
         this.mspRedactJSON = '{"P":null,"Mat":[[0,0,-1,0],[0,-1,1,0],[0,0,0,-1],[0,-1,0,1],[1,1,0,0]],"RowToAttrib":["0","1","2","3","5"]}'
         this.cid = "QmYpLtwy3gHbg9Jh8ae5g11aQeh7P69KEWttdS7p3NB12j"
-        this.cipherJSON = fs.readFileSync('../cipherJSON.txt','utf8')
+        this.cipherJSON = fs.readFileSync('cipherJSON.txt','utf8')
     }
 
     async submitTransaction() {
